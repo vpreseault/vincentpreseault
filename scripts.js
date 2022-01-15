665714dad4dbf67142e2e059add84aae8dedf0f8
Vincent Preseault <vincent.preseault@gmail.com>
2022-01-15 16:00:42 -0500

    Created main nav filter

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function filter(button, filterOption) {
+function timelineFilter(button, filterOption) {
   // add and remove active class
   let previousActive = document.getElementsByClassName("active");
   for (let i = 0; i < previousActive.length; i++) {
@@ -17,6 +17,17 @@ function filter(button, filterOption) {
       if (timeline[i].id != filterOption) {
         timeline[i].style.display = "none";
       }
+    }
+  }
+}
+
+function navFilter(filterOption) {
+  let sections = document.getElementsByTagName("section");
+  for (let i = 0; i < sections.length; i++) {
+    if (filterOption != sections[i].id) {
+      sections[i].style.display = "none";
+    } else {
+      sections[i].style.display = "block";
     }
   }
 }
